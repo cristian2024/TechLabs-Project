cd1e60f9d630df9836756c4eb0204ce3ce99ecec
Cristian <cristiandramirezm.2024@gmail.com>
2022-01-26 04:15:41 -0800

    Stock e history terminados sin probar

--- routes/product.js
+++ routes/product.js
@@ -4,6 +4,8 @@ const router = express.Router()
 
 const { setUser, authRole } = require( '../middlewares/auth-user/auth' );
 const ROLES = require( '../middlewares/auth-user/roles' );
+const { GetStockId, GetStock, HasStock } = require('../middlewares/others/stock')
+const { CreateStock, GetHistoryProduct } = require('../middlewares/others/product')
 
 
 const Product = require("../models/Product")
@@ -14,7 +16,7 @@ router.get("/list-types",(req,res) => {
   res.send(types)
 })
 
-router.post('/new-product', setUser, authRole([ROLES.ESTABLISHMENT]) ,(req, res) => {
+router.post('/new-product', setUser, authRole([ROLES.ESTABLISHMENT]) ,(req, res, next) => {
   const {
     product_type,
     product_name,
@@ -23,8 +25,10 @@ router.post('/new-product', setUser, authRole([ROLES.ESTABLISHMENT]) ,(req, res)
   } = req.body
   
   try{
+    const establishment_id = req.userData.rol.rol_id
+    req.establishment_id = establishment_id
     const producto = new Product({
-      establishment_id: req.userData.rol.rol_id,
+      establishment_id,
       product_type,
       product_name,
       description,
@@ -38,15 +42,60 @@ router.post('/new-product', setUser, authRole([ROLES.ESTABLISHMENT]) ,(req, res)
 
         res.send({ message: "se presento un error en la creacion del producto", err})
       }else{
-        res.status(200)
-        res.send({ data: producto, completed: true})
+        req.product_id =  product._id
+        req.new_product={
+          completed: true,
+          product
+        }
+        next()
+        // res.status(200)
+        // res.send({ data: producto, completed: true})
       }
     })
   }catch(error){
-    console.log(error)
+    // console.log(error)
     res.status(400)
     res.send({ message: "se presento un error en la creacion del producto", error})
   }
+}, CreateStock)
+
+
+router.put('/update-stock',GetStockId, HasStock, async (req,res)=>{
+  const {
+    amount,
+  } = req.body
+  // middleware 'GetStockId' put the stock_id in the requirements
+  const stock = req.stock_id
+  if(!stock){
+    res.status(400)
+    res.send({error: "it is neccesary to have the id of the stock"})
+  }else{
+    try{
+      if(req.hasStock){
+        Product.findByIdAndUpdate(stock, { $inc: { stock: amount }}, (error, result)=>{
+          if(error){
+            res.status(400)
+            res.send({error: "it is neccesary to have the id of the stock"})
+          }
+          res.status(200)
+          res.send(result)
+          
+        })
+      }else{
+        res.status(400)
+        res.send({error: "Thats an amount not allowed"})
+      }
+    }catch(error){
+      res.status(400)
+      res.send({error})
+    }
+  }
+  
+
 })
+
+router.get('/stock-product', GetStockId, GetStock )
+
+router.get('/history-product', GetStockId, GetHistoryProduct)
 
 module.exports = router
